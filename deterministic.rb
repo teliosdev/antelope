7d67055caa0fcbe1b9690ef97b33d1f56d8e0d88
Jeremy Rodi <redjazz96@gmail.com>
2014-06-06 23:39:57 -0400

    Move output generation to bin file

--- deterministic.rb
+++ deterministic.rb
@@ -25,23 +25,3 @@ class MyParser < Antelope::Parser
     end
   end
 end
-
-recognizer = Antelope::Recognizer.new(MyParser)
-recognizer.call
-c = Antelope::Constructor.new(MyParser)
-c.call
-
-File.open("deterministic.output", "w") do |f|
-  f << "productions:\n"
-  c.productions.each do |rule|
-    f << "#{Antelope::Output.output(rule)}"
-  end
-
-  f << "\nfollow():\n"
-  c.productions.map { |x| x.left }.uniq.each do |p|
-    f << "  #{p}: #{c.follow(p).to_a.join(" ")}\n"
-  end
-
-  f << "\n"
-  Antelope::Output.output(recognizer.start, f)
-end
--- simple.rb
+++ simple.rb
@@ -9,37 +9,17 @@ class MyParser < Antelope::Parser
 
   productions do
     production :e do
-      match l EQUALS r
+      match l, EQUALS, r
       match r
     end
 
     production :l do
       match IDENT
-      match STAR r
+      match STAR, r
     end
 
     production :r do
       match l
     end
   end
-end
-
-recognizer = Antelope::Recognizer.new(MyParser)
-recognizer.call
-c = Antelope::Constructor.new(MyParser)
-c.call
-
-File.open("simple.output", "w") do |f|
-  f << "productions:\n"
-  c.productions.each do |rule|
-    f << "#{Antelope::Output.output(rule)}"
-  end
-
-  f << "\nfollow():\n"
-  c.productions.map { |x| x.left }.uniq.each do |p|
-    f << "  #{p}: #{c.follow(p).to_a.join(" ")}\n"
-  end
-
-  f << "\n"
-  Antelope::Output.output(recognizer.start, f)
 end
